e87993dfe3e1ab6389a8c37dcdf478058cbae00f
qinxuyang <1106207638@qq.com>
2022-08-10 12:30:27 +0800

    实现 aside切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="topname">
-    <div class="logo"></div>
-    <ul class="menu">111</ul>
+    <div class="logo" @click="toggleMenu">12321321</div>
+    <ul class="menu">
+      111
+    </ul>
   </div>
 </template>
+<script lang="ts">
+import { inject, Ref } from "vue";
+
+export default {
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    console.log(menuVisible.value);
+    const toggleMenu = () => {
+      menuVisible.value = !menuVisible.value;
+    };
+    return { toggleMenu };
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 </style>
--- src/views/doc.vue
+++ src/views/doc.vue
@@ -2,11 +2,11 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>
+      <aside v-if="menuVisible">
         <h2>组件列表</h2>
         <ol>
           <li><router-link to="/doc/swich"> Switch 组件</router-link></li>
-          <li><router-link  to="/doc/button"> Button 组件</router-link></li>
+          <li><router-link to="/doc/button"> Button 组件</router-link></li>
           <li><router-link to="/doc/dialog"> Dialog 组件</router-link></li>
           <li><router-link to="/doc/tabs"> Tabs 组件</router-link></li>
         </ol>
@@ -16,10 +16,16 @@
   </div>
 </template>
 <script lang='ts'>
+import { inject, Ref } from "vue";
 import Topnav from "../components/Topnav.vue";
 export default {
   components: {
     Topnav,
+  },
+  setup() {
+    const menuVisible = inject<Ref<boolean>>("menuVisible");
+    console.log(menuVisible.value);
+    return{menuVisible}
   },
 };
 </script>
