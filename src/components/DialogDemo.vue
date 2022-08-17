34e02ec7714dc68e328937788d9bd1ee21bb9514
qinxuyang <1106207638@qq.com>
2022-08-17 13:11:39 +0800

    初始化Dialog组件信息

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,3 +1,11 @@
 <template>
-    <div>dia</div>
+  <div><h1>示例1</h1></div>
+  <Dialog></Dialog>
 </template>
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: { Dialog },
+  setup() {},
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>内容1</p>
+      <p>内容2</p>
+    </main>
+    <footer><Button>OK</Button><Button>取消</Button></footer>
+  </div>
+</template>
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: {
+    Button,
+  },
+  setup() {},
+};
+</script>
