793c510d8eb69df89f70c7d84fbff8905a64bddf
qinxuyang <1106207638@qq.com>
2022-08-17 22:59:37 +0800

    Dialog支持点击关闭，并且拥有表单验证的雏形

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,11 +1,32 @@
 <template>
   <div><h1>示例1</h1></div>
-  <Dialog></Dialog>
+  <Button @click="toggle">切换</Button>
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancle="f2"
+  ></Dialog>
 </template>
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
 export default {
-  components: { Dialog },
-  setup() {},
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
+      console.log(1);
+      return true;
+    };
+    const f2 = () => {
+      console.log(2);
+    };
+    return { x, toggle, f1, f2 };
+  },
 };
 </script>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,20 +1,63 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>内容1</p>
-      <p>内容2</p>
-    </main>
-    <footer><Button>OK</Button><Button>取消</Button></footer>
-  </div>
+  <template v-if="visible">
+    <div class="dialog-overlay" @click="onClickOverlay"></div>
+    <div class="dialog-wrapper">
+      <header>
+        标题
+        <div @click="close" class="gulu-span-close">222</div>
+      </header>
+      <main>
+        <p>内容1</p>
+        <p>内容2</p>
+      </main>
+      <footer>
+        <Button @click="ok">OK</Button><Button @click="cancle">取消</Button>
+      </footer>
+    </div>
+  </template>
 </template>
+
 <script lang="ts">
 import Button from "./Button.vue";
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true,
+    },
+    ok: {
+      type: Function,
+    },
+    cancle: {
+      type: Function,
+    },
+  },
   components: {
     Button,
   },
-  setup() {},
+  setup(props, context) {
+    const close = () => {
+      console.log(1221);
+      context.emit("update:visible", false);
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close();
+      }
+    };
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close();
+      }
+    };
+    const cancle = () => {
+      context.emit("cancle");
+    };
+    return { close, onClickOverlay, ok, cancle };
+  },
 };
 </script>
